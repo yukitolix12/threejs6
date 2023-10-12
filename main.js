699205b1860aea8710b488797282d7ee042ede99
yukitolix12 <132648775+yukitolix12@users.noreply.github.com>
2023-10-12 13:13:41 +0900

    マウス操作機能作成

--- main.js
+++ main.js
@@ -29,3 +29,7 @@ const material = new THREE.MeshNormalMaterial();
 //ライト
 const ambientLight = new THREE.AmbientLight(0xffffff, 0.8);
 scene.add(ambientLight);
+
+//マウス操作
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
